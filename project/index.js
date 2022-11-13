0044d03670d53524bd4cc8042503ea7b91ea5dcb
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2022-11-13 08:20:06 -0300

    feat: add `ejs` and `html` engine for server

--- project/index.js
+++ project/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const configureLoginRoute = require('./routes/login-route');
 const configureRegisterRoute = require('./routes/register-routes');
+const configureRenderEngine = require('./middleware/engine')
 
 const application = express();
 
+configureRenderEngine(application);
 configureLoginRoute(application);
 configureRegisterRoute(application);
 
---
+++ project/middleware/engine.js
@@ -0,0 +1,15 @@
+const ejs = require('ejs');
+const path = require('path')
+const express = require('express');
+
+function configureRenderEngine(application) {
+    application.engine('html', ejs.renderFile);
+    application.set('view engine', 'html');
+    application.set('view engine', 'ejs');
+    application.set('views', path.join(__dirname, 'views'));
+
+    application.use('/public', express.static(path.join(__dirname, 'public')));
+    application.use(express.static('./public'));
+}
+
+module.exports = configureRenderEngine;
